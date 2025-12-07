2ffd01499a21b646d7fa94f64968494eba278784
ozan <151374410+oz1122@users.noreply.github.com>
2025-12-07 14:17:12 +0700

    Update Supabase client import to use esm.sh

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
     "https://diyxtkreoplqmzkhzgci.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpeXh0a3Jlb3BscW16a2h6Z2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzYyNTIsImV4cCI6MjA4MDAxMjI1Mn0.Nrezrpt39WBNYLJ6LvzKx_X0s5wcXbRWMvReglsfoc0"
 );
+
